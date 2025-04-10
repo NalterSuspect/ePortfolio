1d30ab20596198a57ab36112c43b91409839381a
Sebastian <azerdumat@gmail.com>
2025-04-10 23:42:24 +0200

    jumper section + github links

--- public/script/script.js
+++ public/script/script.js
@@ -1,3 +1,12 @@
 function getProject(id){
     document.location.href = "project"+id+".html";
 }
+
+function jumpTo(id){
+    let section = document.getElementById(id);
+    section.scrollIntoView({ behavior: 'smooth' });
+}
+
+function openWindowWithUrl(url){
+    window.open(url, '_blank');
+}
