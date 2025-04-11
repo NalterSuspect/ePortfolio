364d246a31c65b1b8f7a0a495a1276cc7920a90e
Sebastian <azerdumat@gmail.com>
2025-04-11 23:08:11 +0200

    size project when hover

--- public/script/script.js
+++ public/script/script.js
@@ -10,3 +10,14 @@ function jumpTo(id){
 function openWindowWithUrl(url){
     window.open(url, '_blank');
 }
+
+function highlightProject(projectId) {
+    document.querySelectorAll('.image-holder img').forEach(img => {
+      img.style.zIndex = 0;
+    });
+  
+    const img = document.getElementById(`img-${projectId}`);
+    if (img) {
+      img.style.zIndex = 2;
+    }
+  }
